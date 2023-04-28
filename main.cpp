a573c84442184562aa7dc2d049762005133256e5
valeriaconte <128838224+valeriaconte@users.noreply.github.com>
2023-04-28 20:14:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,26 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int lista[10];
+    for(int i=0;i<10;i++){
+        cin >> lista[i];
+    }
+    int a=0;
+    cin >> a;
+    
+            if (lista[9]>a){
+                for (int i=0; i<10; i++){
+                    if (lista[i]>a){
+                        for (int j=9; j>i; --j){
+                            lista[j]=lista[j-1];}
+                            lista[i]=a;
+                            break;   
+                    }
+                }
+            }
+            
+            for(int i=0;i<10;i++){
+        cout << lista[i] << " ";
+    }
+    return 0;
 }
